7e56ff59938119e5a0b4306d345523a77a8e6fce
Alican Demirtas <alican@compositional-it.com>
2020-03-29 12:27:27 +0100

    Basic Event Store implemented.

--- Program.fs
+++ Program.fs
@@ -3,38 +3,31 @@
         { Get : unit -> 'Event list
           Append : 'Event list -> unit }
         
-    module EventStore =
-        // What can the MB do? 
-        type Msg = 
-            | Increment
-            // What kind of reply do we expect
-            | CurrentState of AsyncReplyChannel<int> 
-            
-        let agent =
-            MailboxProcessor.Start(fun inbox ->
-                // state can be any state the agent should store
-                let rec loop state =     
-                    async {
-                        let! msg = inbox.Receive()
-                        match msg with
-                        // call the recursive function to let the agent live
-                        | Increment -> return! loop (state + 1) 
-                        | CurrentState reply ->
-                            //reply on the given channel 
-                            reply.Reply state 
-                            // call the recursive function to let the agent live
-                            return! loop state }
-                loop 0 )
-        
-        agent.Post Increment
-        // Because `CurrentState` itself is a function itself, we need not use a lambda expression.
-        let value = agent.PostAndReply CurrentState
+    module EventStore = 
+        type Msg<'Event> =  // What can the MB do? 
+            | Append of 'Event list
+            | Get of AsyncReplyChannel<'Event list> // What kind of reply do we expect
+        let initialize () : EventStore<'Event> =
+            let agent =
+                MailboxProcessor.Start(fun inbox ->
+                    let rec loop history =  // state can be any state the agent should store
+                        async
+                            { let! msg = inbox.Receive()
+                              match msg with
+                              | Append events ->
+                                  return! loop (history @ events)
+                              | Get reply -> 
+                                  reply.Reply history  //reply on the given channel
+                                  return! loop history }  // call the recursive function to let the agent live
+                    loop [])
+            let append events = agent.Post (Append events)
+            let get () = agent.PostAndReply Get  // Because `CurrentState` itself is a function itself, we need not use a lambda expression.
+            { Get = get; Append = append }
        
 module Domain =
     type Flavour =
-    | Strawberry
-    | Vanilla
-
+        | Strawberry
+        | Vanilla
     type Event =
         | Flavour_sold of Flavour
         | Flavour_restocked of Flavour * int
@@ -42,15 +35,9 @@ module Domain =
         | Flavour_was_not_in_stock of Flavour
 
 module Helper =
-    let printUl list =
-        list
-        |> List.iteri (fun i item -> printfn " %i: %A" (i + 1) item)
-        
+    let printUl list = list |> List.iteri (fun i item -> printfn " %i: %A" (i + 1) item)
     let printEvents events =
-        events
-        |> List.length
-        |> printfn "History (Length %i)"
-        
+        events |> List.length |> printfn "History (Length %i)"
         events |> printUl
 
 open Infrastructure
@@ -58,15 +45,14 @@ open Domain
 open Helper
 
 [<EntryPoint>]
-let main argv =
+let main _ =
     let eventStore : EventStore<Event> = EventStore.initialize()
     
     eventStore.Append [ Flavour_restocked (Vanilla, 3) ]
-    eventStore.Append [ Flavour_sold (Vanilla) ]
-    eventStore.Append [ Flavour_sold (Vanilla) ]
-    eventStore.Append [ Flavour_sold (Vanilla) ]
+    eventStore.Append [ Flavour_sold Vanilla ]
+    eventStore.Append [ Flavour_sold Vanilla ]
+    eventStore.Append [ Flavour_sold Vanilla; Flavour_went_out_of_stock Vanilla ]
     
-    eventStore.Get() |> printEvents
-    
-    printfn "Hello World from F#!"
+    let events = eventStore.Get()
+    events |> printEvents
     0 // return an integer exit code
