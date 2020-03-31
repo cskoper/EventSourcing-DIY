1fa153bef24adbcbd13463364be095fafaca261e
Alican Demirtas <alican@compositional-it.com>
2020-03-31 15:44:33 +0100

    A 'Projection' type and some functions to work with it has been implemented.

--- Program.fs
+++ Program.fs
@@ -4,25 +4,29 @@
           Append : 'Event list -> unit }
         
     module EventStore = 
-        type Msg<'Event> =  // What can the MB do? 
+        type Msg<'Event> =  //What can the MB do? 
             | Append of 'Event list
-            | Get of AsyncReplyChannel<'Event list> // What kind of reply do we expect
+            | Get of AsyncReplyChannel<'Event list>  //What kind of reply do we expect
+            
         let initialize () : EventStore<'Event> =
             let agent =
                 MailboxProcessor.Start(fun inbox ->
-                    let rec loop history =  // state can be any state the agent should store
-                        async
-                            { let! msg = inbox.Receive()
-                              match msg with
-                              | Append events ->
-                                  return! loop (history @ events)
-                              | Get reply -> 
-                                  reply.Reply history  //reply on the given channel
-                                  return! loop history }  // call the recursive function to let the agent live
+                    let rec loop history =  //State can be any state the agent should store.
+                        async {
+                            let! msg = inbox.Receive()
+                            match msg with
+                            | Append events ->
+                                return! loop (history @ events)
+                            | Get reply -> 
+                                reply.Reply history  //Reply on the given channel.
+                                return! loop history }  //Call the recursive function to let the agent live.
                     loop [])
+            
             let append events = agent.Post (Append events)
-            let get () = agent.PostAndReply Get  // Because `CurrentState` itself is a function itself, we need not use a lambda expression.
-            { Get = get; Append = append }
+            let get () = agent.PostAndReply Get  //Because `CurrentState` itself is a function itself, we need not use a lambda expression.
+            
+            { Get = get
+              Append = append }
        
 module Domain =
     type Flavour =
@@ -33,16 +37,50 @@ module Domain =
         | Flavour_restocked of Flavour * int
         | Flavour_went_out_of_stock of Flavour
         | Flavour_was_not_in_stock of Flavour
+        
+module Projections =
+    open Domain
+    
+    type Projection<'State,'Event> =
+        { Init : 'State
+          Update : 'State -> 'Event -> 'State }
+        
+    let project (projection : Projection<_,_>) events =
+        events |> List.fold projection.Update projection.Init
+        
+    let soldOfFlavour flavour state =
+        state |> Map.tryFind flavour |> Option.defaultValue 0
+        
+    let updateSoldFlavours state event =
+        match event with
+        | Flavour_sold flavour ->
+            state
+            |> soldOfFlavour flavour
+            |> fun portions ->
+                state |> Map.add flavour (portions + 1)  //Map.add will update if key already exists.
+        | _ -> state
+        
+    let soldFlavours : Projection<Map<Flavour,int>,Event> =
+        { Init = Map.empty
+          Update = updateSoldFlavours }
 
 module Helper =
-    let printUl list = list |> List.iteri (fun i item -> printfn " %i: %A" (i + 1) item)
+    open Projections
+    
+    let printUl list =
+        list |> List.iteri (fun i item -> printfn " %i: %A" (i + 1) item)
+    
     let printEvents events =
-        events |> List.length |> printfn "History (Length %i)"
+        events |> List.length |> printfn "History (Length: %i)"
         events |> printUl
-
+    
+    let printSoldFlavour flavour state =
+        state |> soldOfFlavour flavour |> printfn "Sold %A %i" flavour
+     
 open Infrastructure
 open Domain
 open Helper
+open Projections
 
 [<EntryPoint>]
 let main _ =
@@ -55,4 +93,9 @@ let main _ =
     
     let events = eventStore.Get()
     events |> printEvents
-    0 // return an integer exit code
+    
+    let sold : Map<Flavour,int> = project soldFlavours events
+    
+    printSoldFlavour Vanilla sold
+    printSoldFlavour Strawberry sold
+    0 //return an integer exit code
